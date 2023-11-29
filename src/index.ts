ea283e203a834d80a1b68fbe26492d50ff094940
Xiangyi Li <venusli370306@gmail.com>
2023-11-29 14:27:36 -0800

    Added API client generation with openapi-typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import createClient from "openapi-fetch";
 import OpenAIClient from '@/lib/OpenAIClient';
+import { paths } from "@/openaiapi";
 
 const apiKey = process.env.OPENAI_API_KEY!;
-
 
 async function getCompletionDirectly() {
     const url = "https://api.openai.com/v1/chat/completions";
@@ -37,6 +38,24 @@ async function getCompletionWithClient() {
     }
 }
 
+async function getCompletinoWithGeneratedClient() {
+    const { GET, POST } = createClient<paths>({ baseUrl: "https://api.openai.com/v1/" });
+
+    const { data, error } = await POST("/chat/completions", {
+        body: {
+            model: "gpt-3.5-turbo",
+            max_tokens: 1000,
+            messages: [{ role: 'user', content: 'What is OpenAPI' }],
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        }
+    });
+    console.log(data!.choices[0].message);
+}
+
 
 getCompletionDirectly();
 getCompletionWithClient();
+getCompletinoWithGeneratedClient();
